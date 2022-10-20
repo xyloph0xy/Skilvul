222d8b3ce9ab74e090bf196fe0385387e57437a1
xyloph0xy <sofiyahluluk7@gmail.com>
2022-10-20 15:17:18 +0700

    new code

--- TPA/MovieAppwithTMDBAPI/script.js
+++ TPA/MovieAppwithTMDBAPI/script.js
@@ -1,18 +1,66 @@
-fetch(`https://api.themoviedb.org/3/discover/movie?api_key=9ce85ed406f6b2e4420bb68fe84eb2a1&language=en-US&sort_by=popularity.desc&page=1`)
+let search= document.getElementById("search")
+let show= document.getElementById("movie-app")
+search.onkeyup = function(event) {
+  if (event.target.value == '') {
+      show.innerHTML = ``;
+      listMovie();
+  }else{
+      show.innerHTML = ``
+      let search_term = event.target.value;
+      console.log(search_term);
+          fetch(
+              'https://api.themoviedb.org/3/search/movie?'+
+              new URLSearchParams ({
+                  api_key : "9ce85ed406f6b2e4420bb68fe84eb2a1",
+                  query: search_term,
+                  page: 1,
+              }) 
+              )
+              .then((result) => result.json())
+              .then((result) => {
+                  console.log(result);
+                  show.innerHTML = ''
+                  result.results.forEach(item => {
+                    console.log(item);
+            
+                    show.innerHTML += 
+
+                    `
+                    
+                    <div class="col ">
+                    <div class="card h-100 ">
+                      <img src="https://www.themoviedb.org/t/p/w500${item.poster_path}
+                      " class="card-img-top" alt="" />
+                      <div class="row p-2" >
+                        <h5 class="card-title col-10">${item.title}</h5>
+                        <p class="card-text col-2"><strong>${item.vote_average}</strong></p>
+                        <small class="text-muted ">${item.release_date}</small>
+                      </div>
+                      
+                    </>
+                  </div>
+                  </div>
+                    `
+                });
+              
+
+          });
+  }
+};
+function listMovie(){
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=9ce85ed406f6b2e4420bb68fe84eb2a1&language=en-US&sort_by=popularity.desc&page=1`)
 .then(result => {
-    console.log(result);
     return result.json()
     // console.log(result);
 })
 .then(result =>{
-    console.log(result)
-    console.log(result.results);
+
+  
     let movies = result.results;
 
     movies.forEach(item => {
-        console.log(item);
+  
 
-        let show= document.getElementById("movie-app")
         show.innerHTML += 
         `
         
@@ -32,6 +80,8 @@ fetch(`https://api.themoviedb.org/3/discover/movie?api_key=9ce85ed406f6b2e4420bb
         `
     });
 })
+}
+listMovie();
 
 
 // ==================================================================
