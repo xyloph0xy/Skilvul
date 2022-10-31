ae0eb148a321ee31c4ef3ffa1d5ae5b0fe43b9a9
xyloph0xy <sofiyahluluk7@gmail.com>
2022-11-01 03:35:43 +0700

    push week3

--- Writing & Presentasi Test/Bootcamp/coba.js
+++ Writing & Presentasi Test/Bootcamp/coba.js
@@ -5,16 +5,16 @@ app.use(express.json());
 //.json digunakan untuk parsing request ke json ketika melakukan post
 //app.use digunakan untuk menggunakan function middleware
 
+const biodata =  [
+   {
+      nama: "Luluk",
+      tempat: "Magelang",
+      tanggal: "20 Juli 2002"
+   }
+]
+
 app.get("/biodata",(req,res)=>{
-   res.send(
-      [
-         {
-            nama: "Luluk",
-            tempat: "Magelang",
-            tanggal: "20 Juli 2002"
-         }
-      ]
-   )
+   res.send(biodata)
 })
 
 app.post("/biodata",(req,res)=>{
@@ -22,11 +22,12 @@ app.post("/biodata",(req,res)=>{
    //untuk menggunakan post diperlukan adanya middleware
    const data = req.body;
    biodata.push(data);
-   // console.log(data);
+   //digunakan untuk menambahkan data respone yang di request oleh user
+   res.send("oke")
    //menampilkan data yang dikirim oleh user ke dalam terminal
    res.status(201)
    //digunakan untuk memberi status code
-   res.send("oke")
+   
 })
 
 app.listen(7000);
