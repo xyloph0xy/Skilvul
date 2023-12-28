4679ccdc94863da08cfe3825f0ae21a7c53024ba
LulukSofiyah <92715425+xyloph0xy@users.noreply.github.com>
2023-12-28 13:43:13 +0700

    Update script.js

--- TPA/MovieAppwithTMDBAPI/script.js
+++ TPA/MovieAppwithTMDBAPI/script.js
@@ -83,19 +83,3 @@ function listMovie(){
 listMovie();
 
 
-// ==================================================================
-// async function movieApp() {
-//         let response = await fetch ('https://api.themoviedb.org/3/discover/movie?api_key=9ce85ed406f6b2e4420bb68fe84eb2a1&language=en-US&sort_by=popularity.desc&page=1')
-//         let result = await response.json()
-//         console.log(result)
-//         // console.log(response)
-//         result.forEach(item => {
-//             console.log(item);
-//         });
-
-        
-// }
-// movieApp()
-
-// ===================================================================
-
